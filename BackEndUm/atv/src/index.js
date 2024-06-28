e2b669d170cf299efb4d8e55b0a25d80eba0a896
Renner-Mk <patrickrenner.m@gmail.com>
2024-06-28 20:29:06 -0300

    mudança

---
+++ BackEndUm/atv/src/index.js
@@ -0,0 +1,20 @@
+const num1 = 5
+const num2 = 10
+console.log("OIEasdasdEE")
+console.log(`A soma é: ${num1+num2}`)
+
+//     CRUD - (CREATE - READ - UPDATE - DELETE)
+// GET --> READ
+// POST --> CREATE
+// PUT --> UPDATE
+// DELETE --> DELETE
+// rodar no packeg
+// "start": "node src/index.js" npm start
+// "dev": "node src/index.js" npm run dev
+// yarn gerenciador de pacotes procurar e verifiacar mais tarde
+// nodemon src/index.js   para iniciar se nao tiver o nodemon no package
+
+// ctrl + c para parar a execução
+
+
+
