ad0b9e81b24e64f70477ca746655678e31d3d328
Renner-Mk <patrickrenner.m@gmail.com>
2024-07-01 21:31:09 -0300

    aula

---
+++ BackEndUm/aula28-6/src/potencia.js
@@ -0,0 +1,10 @@
+const x = 10
+const y = 6
+const z = 3
+
+console.log(`A potência do numero ${x} e ${x ** 2}, ${y} e ${y ** 2}, ${z} e ${z ** 2}`)
+
+// npm i -D nodemon sucrase
+// criar pasta nodemon.json e passar o codigo
+// npm run dev 
+// npm sempre vai ser usado quando estiver no script
