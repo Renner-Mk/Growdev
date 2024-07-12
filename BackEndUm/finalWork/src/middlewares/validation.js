1982a47307f994a118c3a9e3336e4aa8f93ddee0
Renner-Mk <patrickrenner.m@gmail.com>
2024-07-11 23:00:49 -0300

    criação do login do usuario e ajustes

--- BackEndUm/finalWork/src/middlewares/validation.js
+++ BackEndUm/finalWork/src/middlewares/validation.js
@@ -1,4 +1,4 @@
-import { response } from "express"
+import e, { response } from "express"
 import {users} from '../routes/users'
 
 export function validateUserRestration(request, response, next){
@@ -6,27 +6,34 @@ export function validateUserRestration(request, response, next){
     const user = users.find(user => user.email === email)
 
     if(!name){
-        return response.status(400).json({
-            message: 'Por favor, verifique se passou o nome.'
-        })
+        return response.status(400).send('Por favor, verifique se passou o nome.'
+        )
     }
     if(!email){
-        return response.status(400).json({
-            message: 'Por favor, verifique se passou o email.'
-        })
+        return response.status(400).send('Por favor, verifique se passou o email.'
+        )
     }
     
     if(user){
-        return response.status(400).json({
-            message: 'Email já cadastrado, insira outro.'
-        })
+        return response.status(400).send('Email já cadastrado, insira outro.')
     }
 
     if(!password){
-        return response.status(400).json({
-            message: 'Por favor, verifique se passou a senha.'
-        })
+        return response.status(400).send('Por favor, verifique se passou a senha.')
     }
 
     next()
+}
+
+export function validateUserLogin(request, response, next){
+    const { email, password} = request.body
+
+    if(!email){
+        response.status(400).send('Insira um e-mail válido.')
+    }
+    if(!password){
+        response.status(400).send('Insira uma senha válida.')
+    }
+    
+    next();
 }
--- BackEndUm/finalWork/src/routes/users.js
+++ BackEndUm/finalWork/src/routes/users.js
@@ -1,29 +1,60 @@
 import express from "express";
 import bcrypt from 'bcrypt';
 import {v4 as uuidv4} from 'uuid'
-import { validateUserRestration } from "../middlewares/validation";
+import { validateUserLogin, validateUserRestration } from "../middlewares/validation";
 
 const router = express.Router();
 
 export const users = []
 
 router.post('/signup', validateUserRestration, async (request, response) =>{
-    const {name, email, password} = request.body
+    try {
+        const {name, email, password} = request.body
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+        const hashedPassword = await bcrypt.hash(password, 10);
 
-    const newUser = {
-        id: uuidv4(),
-        name,
-        email,
-        password: hashedPassword
+        const newUser = {
+            id: uuidv4(),
+            name,
+            email,
+            password: hashedPassword
+        }
+
+        users.push(newUser)
+
+        response.status(201).json(`Seja bem vindo ${name}! Pessoa usuária registrada com sucesso!`)
+
+    } catch {
+        response.status(500).json({
+            message: 'Erro ao entrar na pagina.'
+        });
     }
-
-    users.push(newUser)
-
-    response.status(201).json({
-        message: `Seja bem vindo ${name} ! Pessoa usuária registrada com sucesso!`
-    })
 })
+
+router.post('/login', validateUserLogin, async (request, response) =>{
+    try{
+        const {email, password} = request.body
+
+        const user = users.find(user => user.email === email)
+
+        if(!user){
+            return response.status(404).send("Email não encontrado no sistema, verifique ou crie uma conta")
+        }
+
+        const passwordMatch = await bcrypt.compare(password, user.password)
+
+        if(!passwordMatch){
+            return response.status(404).send("Credenciais invalidas")
+        }
+
+        response.status(200).send(`Seja bem vindo ${user.name} ! Pessoa usuária logada com sucesso!`)
+    }catch{
+        response.status(500).json({
+            message: 'Erro ao entrar na pagina.'
+        });
+    }
+})
+
+
 
 export default router
